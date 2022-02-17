a126ffae8efc4bb7872f6444ea85e0ca249a73a5
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2022-02-17 04:29:56 +0300

    Добавлен мигратор + протестирован мигратор (применена одна миграция)

---
+++ Migrator/Migrations/CreateUpdatesTableMigration_202202170403.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Migrator.Migrations
+{
+    [Migration(202202170403)]
+    public class CreateUpdatesTableMigration_202202170403 : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("updates")
+                .WithColumn("update_id").AsInt32().PrimaryKey();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("updates");
+        }
+    }
+}
